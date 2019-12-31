959e63bab07aa53c935df79d9459053bdbe839f3
Audrius Kniuras <audrius.kniuras@intermedix.com>
2019-12-31 17:01:21 +0200

    day3unf

--- day3.py
+++ day3.py
@@ -8,11 +8,29 @@ grid[13][1] = "O"
 
 FIRST_WIRE = FIRST_WIRE.split(",")
 
-for path in FIRST_WIRE:
-    curr_y = 13
-    curr_x = 1
 
-    
+curr_y = 13
+curr_x = 1
+
+for path in FIRST_WIRE:
+    if path[0] == "R":
+        i = 0
+        right = int(path[1])
+        while right > 0:
+            curr_x += 1
+            grid[curr_y][curr_x] = "-"
+            right -= 1
+        grid[curr_y][curr_x] = "+"
+    if path[0] == "U":
+        i = 0
+        up = int(path[1])
+        while up > 0:
+            curr_y -= 1
+            grid[curr_y][curr_x] = "|"
+            up -= 1
+        grid[curr_y][curr_x] = "+"
+    #TODO...
+            
 
 for row in grid:
     print(row)
