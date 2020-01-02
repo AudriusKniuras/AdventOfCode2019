e38f35c83888cf666487e44bad023369bb7d4401
AudriusKniuras <kn.audrius@gmail.com>
2020-01-02 21:33:01 +0200

    day6

---
+++ day6.py
@@ -0,0 +1,8 @@
+PUZZLE_INPUT = open('day6.in').read()
+PUZZLE_INPUT = PUZZLE_INPUT.split('\n')
+
+map_dictionary = {}
+for orbit in PUZZLE_INPUT:
+    first_planet = orbit.split(')')[0]
+    second_planet = orbit.split(')')[1]
+    map_dictionary
