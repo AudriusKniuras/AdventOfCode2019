0727020c767895c1ddfc38bcd507974d6d2a2dc6
AudriusKniuras <kn.audrius@gmail.com>
2020-01-06 21:00:46 +0200

    day6 part2 almost

--- day6.py
+++ day6.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10000)
+
 PUZZLE_INPUT = open('day6_part2.in').read()
 #PUZZLE_INPUT = open('day6.in').read()
 PUZZLE_INPUT = PUZZLE_INPUT.split('\n')
@@ -21,7 +24,7 @@ def find_branches(root_branch, nodes = PUZZLE_INPUT):
     for node in nodes:
         first_part, second_part = node.split(')')
         if first_part == root_branch:
-            branches[root_branch].append(first_part)
+            branches[root_branch].append(second_part)
     return branches
 
 def find_parent_node(child_branch, nodes = PUZZLE_INPUT):
@@ -87,19 +90,52 @@ def visit_branches(node, nodes = PUZZLE_INPUT):
 YOU_parent_node = find_parent_node('YOU')
 
 visited_branches = []
-def find_santa(child_node, PUZZLE_INPUT):
+steps_taken = 0
+def find_santa(node):
     global visited_branches
-    # dict {CHILD: PARENT}
-    parent_child_dict = find_parent_node(child_node)
-    # dict {PARENT: [CHILD1, CHILD2]}
+    global steps_taken
+    # dict {NODE: PARENT}
+    parent_child_dict = find_parent_node(node)
+    # dict {NODE: [CHILD1, CHILD2]}
     node_with_branches = find_branches(list(parent_child_dict.keys())[0])
 
-    parent_node = list(node_with_branches.keys())[0]
-    branches = node_with_branches[parent_node]
+    # "YOU"
+    node = list(node_with_branches.keys())[0]
+    # "K"
+    if len(parent_child_dict[node]) > 0:
+        parent_of_node = parent_child_dict[node][0]
+    else:
+        return
+    # ["BRANCH1", BRANCH2]
+    child_branches = node_with_branches[node]
+    print(f"Current node: {node}")
 
-    if len(branches) == 1:
-        find_santa(parent_node)
-    if len(branches) == 2:
-        for branch in branches:
+    if len(child_branches) == 1:
+        if child_branches[0] not in visited_branches:
+            steps_taken += 1
+            visited_branches.append(child_branches[0])
+            find_santa(child_branches[0])
+        else:
+            find_santa(parent_of_node)
+    if len(child_branches) == 2:
+        not_visited = []
+        for branch in child_branches:
             if branch not in visited_branches:
-                
+                not_visited.append(branch)
+        if len(not_visited) > 0:
+            for not_visited_branch in not_visited:
+                steps_taken += 1
+                visited_branches.append(not_visited_branch)
+                find_santa(not_visited_branch)   
+        else:
+            find_santa(parent_of_node)
+
+    if len(child_branches) == 0:
+        if node == "SAN":
+            print(f"SANTA FOUND, orbit {parent_child_dict}")
+            print(f"STEPS TAKEN: {steps_taken}")
+        else:
+            visited_branches.append(node)
+            find_santa(parent_of_node)
+
+find_santa("YOU")
